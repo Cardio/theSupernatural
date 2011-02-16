cbf0971cc4fda3dd610e09a9e2f1c34e2131d14c
Amanda Jenkins <ajenkin2@mail.umw.edu>
2011-02-15 21:00:30 -0500

    sfljgsdfgh

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Alien Abductions</title>
+<title>theSupernatural</title>
 <link rel="stylesheet" href="styles.css" type="text/css" />
 
 
