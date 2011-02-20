8e7ee5f24e880196cf23f268d23aaae29b901d89
ChicagoTed <dwoodruf@mail.umw.edu>
2011-02-20 09:41:48 -0500

    Updated all existing pages so they'd recognize the session, small update-DW

--- search.php
+++ search.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -9,7 +13,7 @@
 </head>
 <body>
 <?php
-   include('header.html');
+   include('header.php');
 ?>
 
 
