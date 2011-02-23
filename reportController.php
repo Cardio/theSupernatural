a881f70a23f57a4a862ae60de3f7564f8336e653
Amanda Jenkins <ajenkin2@mail.umw.edu>
2011-02-23 13:07:24 -0500

    gsdfgs,dgf

--- reportController.php
+++ reportController.php
@@ -37,11 +37,12 @@ echo "Report Summary: <br/>";
 echo "Name: $name <br/>";
 echo "Date: $month / $day / $yr <br/>";
 echo "The rest is on file. Thank you.<br/>";
-//$query="INSERT INTO sightings (name,city,state,experience,creature_type,action) VALUES ('$name','$city','$state','$exp,'$ctype','$act')";
+
+$query="INSERT INTO sightings (name,city,state,experience,creature_type,action) VALUES ('$name','$city','$state','$exp,'$ctype','$act')";
 //$query2="SELECT * FROM sigthings";
 
-//$result = mysqli_query($db, $query) or die("Error Querying Database");
-//mysqli_close($db);
+$result = mysqli_query($db, $query) or die("Error Querying Database");
+   mysqli_close($db);
 ?>
 
 </div>
