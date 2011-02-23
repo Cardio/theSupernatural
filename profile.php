6b9571d8278deb61dc6565a1794b9aa2af31bbba
Samantha Whay <swhay@mail.umw.edu>
2011-02-23 12:23:08 -0500

    edit account stuff

---
+++ profile.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>The Supernatural--Viewing A User Profile</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+</head>
+<body>
+<?php 
+include('header.php'); 
+?>
+<div id="wrap">
+	<div class="pagewrapper">
+		<div class="innerpagewrapper">
+			<div class="page">
+				<div class="content">
+					<!-- CONTENT -->
+<h3>//put the person's username here</h3>
+<br/>
+<p>Zip Code: //put the person's zip code here</p>
+<p>Bio: //put a short bio here, but add it to the table first</p>
+<p>More info, maybe a picture, the type of things they hunt, reports, etc.</p>
+
+<!-- END CONTENT -->
+</div>
+<?php
+include('SIDEnFOOTER.html');
+?>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
