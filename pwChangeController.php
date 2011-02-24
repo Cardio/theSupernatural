0ad37292dec5d0eec78bf02c2c9146d23c5d5bfc
ChicagoTed <dwoodruf@mail.umw.edu>
2011-02-24 10:25:39 -0500

    Edit account/password works again.-DW

--- pwChangeController.php
+++ pwChangeController.php
@@ -8,21 +8,14 @@
    if ($pw1==$pw2){
    	$query="UPDATE users SET password= SHA('$pw1') WHERE username='$username'";
    	$result = mysqli_query($db, $query);
-<<<<<<< HEAD
 	mysqli_close($db);	
-	header( 'Location: index.php');
+	header( 'Location: editAccount.php?msg=passChange');
    	exit;
    }
    else{
-	echo "The passwords you entered are not the same. Go back and try again.";
 	mysqli_close($db);
+	header( 'Location: editAccount.php?msg=passWrong');
+   	exit;
    }
    
-
-=======
-   }
-   mysqli_close($db);
-  header('Location: editAccount.php?msg=passChange');
-  exit;
->>>>>>> 8780d4b58518c768ca989c24ab379c0503e4ea4a
 ?>
