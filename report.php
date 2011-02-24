59b075238e7855048fe62fae8814acc0c4069287
ChicagoTed <dwoodruf@mail.umw.edu>
2011-02-24 11:08:39 -0500

    Report Sighting now displays the current date by default-DW

--- report.php
+++ report.php
@@ -14,6 +14,8 @@ session_start();
 <body>
 <?php
    include('header.php');
+
+
 ?>
 
 
@@ -31,9 +33,9 @@ session_start();
 					<form method="post" action="reportController.php">
 					<table>
 					<tr><td>Name</td><td><?php echo($_SESSION['username']); ?></td></tr>
-					<tr><td>Date of Sighting</td><td><input type="number" name="month" min="1" max="12" step="1" value="1" size="3"/>
-					<input type="number" name="day" min="1" max="31" step="1" value="1" size="3"/>
-					<input type="number" name="year" min="2009" max="2014" step="1" value="2011" size="4"/></td></tr>
+					<tr><td>Date of Sighting</td><td><input type="number" name="month" min="1" max="12" step="1" value="<?php echo date("m", time()); ?>" size="3"/>
+					<input type="number" name="day" min="1" max="31" step="1" value="<?php echo date("d", time()); ?>" size="3"/>
+					<input type="number" name="year" min="1900" max="2014" step="1" value="<?php echo date("Y", time()); ?>" size="4"/></td></tr>
 					
 					
 					<tr><td>Location (city)</td><td><input type="text" name="city" id="city"/> 
@@ -43,10 +45,61 @@ session_start();
 					<option>AK</option>
 					<option>AS</option>
 					<option>AZ</option>
+					<option>AR</option>
 					<option>CA</option>
+					<option>CO</option>
+					<option>CT</option>
+					<option>DE</option>
+					<option>DC</option>
+					<option>FM</option>
+					<option>FL</option>
+					<option>GA</option>
+					<option>GU</option>
+					<option>HI</option>
+					<option>ID</option>
+					<option>IL</option>
+					<option>IN</option>
+					<option>IA</option>
+					<option>KS</option>
+					<option>KY</option>
+					<option>LA</option>
+					<option>ME</option>
+					<option>MH</option>
+					<option>MD</option>
+					<option>MA</option>
+					<option>MI</option>
+					<option>MN</option>
+					<option>MS</option>
+					<option>MO</option>
+					<option>MT</option>
+					<option>NE</option>
+					<option>NV</option>
+					<option>NH</option>
+					<option>NJ</option>
 					<option>NM</option>
+					<option>NY</option>
+					<option>NC</option>
+					<option>ND</option>
+					<option>MP</option>
+					<option>OH</option>
+					<option>OK</option>
+					<option>OR</option>
+					<option>PW</option>
+					<option>PA</option>
+					<option>PR</option>
+					<option>RI</option>
+					<option>SC</option>
+					<option>SD</option>
+					<option>TN</option>
 					<option>TX</option>
+					<option>UT</option>
+					<option>VT</option>
+					<option>VI</option>
 					<option>VA</option>
+					<option>WA</option>
+					<option>WV</option>
+					<option>WI</option>
+					<option>WY</option>
 					</select></td></tr>
 					
 					<tr><td>Experience</td><td><input type="text" name="exp" size="50" value="Describe your Experience."/></td></tr>
