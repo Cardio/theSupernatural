19c08f8e98244be25866123c28abf401b2860d79
ChicagoTed <dwoodruf@mail.umw.edu>
2011-02-23 22:12:21 -0500

    Minor update, editAccount now redirects.-DW

--- pwChangeController.php
+++ pwChangeController.php
@@ -8,11 +8,8 @@
    if ($pw1==$pw2){
    	$query="UPDATE users SET password= SHA('$pw1') WHERE username='$username'";
    	$result = mysqli_query($db, $query);
-	echo "How do I redirect?";	
-   }
-   else{
-	echo "The passwords you entered are not the same. Go back and try again.";
    }
    mysqli_close($db);
-
+  header('Location: editAccount.php?msg=passChange');
+  exit;
 ?>
