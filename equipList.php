0067aff2d21eb0fe7be4013ea2ffc9e8ed98bac5
ChicagoTed <dwoodruf@mail.umw.edu>
2011-03-20 19:46:50 -0400

    Progress with add equipment-DW

--- equipList.php
+++ equipList.php
@@ -6,7 +6,7 @@ session_start();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Supernatural Sightings</title>
+<title>Supernatural Equipment</title>
 <link rel="stylesheet" href="styles.css" type="text/css" />
 
 
